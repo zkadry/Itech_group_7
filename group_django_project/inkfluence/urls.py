505ad2c9caf921378cf8b95af1c96d32c0ec5ad2
liuhuan <2804936L@student.gla.ac.uk>
2024-03-08 23:20:28 +0000

    add some url apis

--- group_django_project/inkfluence/urls.py
+++ group_django_project/inkfluence/urls.py
@@ -3,8 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login_view, name='login'),
+    path('', views.login_view, name='default'),
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+
+    path('home/', views.home_view, name='home'),
+    path('profile/', views.profile_view, name='profile'),
+
+    path('story_submission/', views.story_submission_view, name='story_submission'),
+    path('story/', views.story_view, name='story'),
+    path('comment/', views.comment_view, name='comment'),
 ]
